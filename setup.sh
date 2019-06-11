c805c3388874c31a2026cbaf4c2e30c225e515f6
John Sharp <johns@contentmaster.com>
2019-06-11 17:06:13 +0100

    debugging

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
-GIT_USERNAME=gitName$RANDOM
+ID=$RANDOM
+GIT_USERNAME=gitName$ID
 GIT_PASSWORD=Pa55w.rd
 
 dotnet build
  
 az webapp deployment user set --user-name $GIT_USERNAME --password $GIT_PASSWORD
 
-az appservice plan create --name educationAppServicePlan --resource-group sqlrg --sku B1 --is-linux
+az appservice plan create --name educationAppServicePlan$ID --resource-group sqlrg --sku B1 --is-linux
 
-az webapp create --resource-group sqlrg --plan educationAppServicePlan --name educationapp --runtime "DOTNETCORE|2.1" --deployment-local-git
+az webapp create --resource-group sqlrg --plan educationAppServicePlan$ID --name educationapp$ID --runtime "DOTNETCORE|2.1" --deployment-local-git
 
-git remote add azure https://$GIT_USERNAME@educationapp.scm.azurewebsites.net/educationapp.git
+git remote add azure https://$GIT_USERNAME@educationapp$ID.scm.azurewebsites.net/educationapp$ID.git
 
-git push azure master
+git push https://$GIT_USERNAME:$GIT_PASSWORD@educationapp$ID.scm.azurewebsites.net/educationapp$ID.git
 
 echo '**DONE**'
